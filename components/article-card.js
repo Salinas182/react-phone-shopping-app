ffb834fc0146ca3016c576cde5ecf3d57c8bd036
Rojiblanco182 <salinas.182@hotmail.com>
2023-03-20 23:53:44 +0100

    improved appearance

--- components/article-card.js
+++ components/article-card.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function ArticleCard({ article }) {
   return (
     <div className="flex flex-col flex-1 group relative border-solid border-2 border-sky-100">
-      <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+      <div className="min-h-80 scale-75 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 sm:scale-50 lg:aspect-none lg:h-80">
         <img src={article.imgUrl} alt={article.model} className="h-full w-full object-cover object-center lg:h-full lg:w-full"></img>
       </div>
-      <div className="mt-4 text-center">
-        <p className="text-sm font-medium text-gray-900 mt-4 mb-2">{article.brand}</p>
+      <div className="mt-1 text-center">
+        <p className="text-sm font-medium text-gray-900 mb-2">{article.brand}</p>
         <div className="mb-2">
           <h3 className="text-sm text-gray-700">
             <Link href={`/articles/${article.id}`}>
--- components/article-details.js
+++ components/article-details.js
@@ -23,23 +23,25 @@ export function ArticleDetails({ article }) {
   }
 
   return (
-    <div className="flex flex-1 group relative border-solid border-2 border-sky-100">
-      <div className="w-1/2 overflow-hidden rounded-md bg-gray-200">
+    <div className="flex flex-col flex-1 group relative border-solid border-2 border-sky-100 lg:flex-row">
+      <div className="basis-1/2 scale-75 overflow-hidden rounded-md bg-gray-200 sm:scale-50">
         <img src={article.imgUrl} alt={article.model} className="h-full w-full object-cover object-center lg:h-full lg:w-full"></img>
       </div>
-      <div className="mt-4 text-center w-1/2">
-        <p className="text-sm font-medium text-gray-900 mt-2">Marca: {article.brand}</p>
-        <p className="text-sm font-medium text-gray-900">Modelo: {article.model}</p>
-        <p className="text-sm font-medium text-gray-900">CPU: {article.cpu}</p>
-        <p className="text-sm font-medium text-gray-900">RAM: {article.ram}</p>
-        <p className="text-sm font-medium text-gray-900">OS: {article.os}</p>
-        <p className="text-sm font-medium text-gray-900">Resolución de pantalla: {article.displayResolution}</p>
-        <p className="text-sm font-medium text-gray-900">Batería: {article.battery}</p>
-        <p className="text-sm font-medium text-gray-900">Cámara principal: {article.primaryCamera}</p>
-        <p className="text-sm font-medium text-gray-900">Cámara secundaria: {article.secondaryCmera}</p>
-        <p className="text-sm font-medium text-gray-900">Dimensiones: {article.dimentions}</p>
-        <p className="text-sm font-medium text-gray-900">Peso: {article.weight}</p>
-        <p className="text-sm font-medium text-gray-900 mb-2">Precio: {article.price} €</p>
+      <div className="basis-1/2 mt-4 text-center">
+        <div className='h-1/2'>
+          <p className="text-sm font-medium text-gray-900 mt-2">Marca: {article.brand}</p>
+          <p className="text-sm font-medium text-gray-900">Modelo: {article.model}</p>
+          <p className="text-sm font-medium text-gray-900">CPU: {article.cpu}</p>
+          <p className="text-sm font-medium text-gray-900">RAM: {article.ram}</p>
+          <p className="text-sm font-medium text-gray-900">OS: {article.os}</p>
+          <p className="text-sm font-medium text-gray-900">Resolución de pantalla: {article.displayResolution}</p>
+          <p className="text-sm font-medium text-gray-900">Batería: {article.battery}</p>
+          <p className="text-sm font-medium text-gray-900">Cámara principal: {article.primaryCamera}</p>
+          <p className="text-sm font-medium text-gray-900">Cámara secundaria: {article.secondaryCmera}</p>
+          <p className="text-sm font-medium text-gray-900">Dimensiones: {article.dimentions}</p>
+          <p className="text-sm font-medium text-gray-900">Peso: {article.weight}</p>
+          <p className="text-sm font-medium text-gray-900 mb-2">Precio: {article.price} €</p>
+        </div>
         <BasicSelect options={article?.options?.colors} label="Color" setValueMethod={setColor} />
         <BasicSelect options={article?.options?.storages} label="Almacenamiento" setValueMethod={setStorage}/>
         <div className='flex justify-center mb-2 content-end mt-4'>
